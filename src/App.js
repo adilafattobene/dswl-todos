5114e634e39b270f525934a40bc5a3176d984f36
afattobene <adila.fattobene@ifood.com.br>
2021-05-06 13:41:44 -0300

    feat: init project and add axios request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    console.log("Fetch Data");
+
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        setTodos(response.data);
+      })
+      .catch((err) => {
+        console.log("Erro", err);
+      });
+  }, []);
+
+  const onTodoSubmit = (event) => {
+    event.preventDefault();
+
+    let newTask = {
+      id: todos.length,
+      title: event.target.title.value,
+    };
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", newTask)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log("Erro", err);
+      });
+  };
+
+  const changeTodo = (event) => {
+    event.preventDefault();
+
+    let id = Number(event.target.id.value);
+
+    let newBooks = todos.slice();
+
+    newBooks[id].title = event.target.title.value;
+
+    setTodos(newBooks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onTodoSubmit}>
+        <input type="text" id="title" placeholder="Digite o título da tarefa" />
+        <input type="submit" id="btnSubmit" value="Cadastrar" />
+      </form>
+      <br />
+      <br />
+      <p>TO DOS</p>
+      {todos.map((todo, index) => {
+        return (
+          <div>
+            <form onSubmit={changeTodo}>
+              <input type="text" id="id" placeholder={index + 1} disabled />
+              <input type="text" id="title" placeholder={todo.title}/>
+              <input type="submit" id="btnSubmit" value="Editar" />
+            </form>
+          </div>
+        );
+      })}
     </div>
   );
 }
